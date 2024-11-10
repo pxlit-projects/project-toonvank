12c6c80ecb4bf2b6e3d6b54003f81a30ff28845b
Toon <toonvankimmenade@gmail.com>
2024-11-10 17:35:27 +0100

    Project structure recreation

--- backend-jav/NewsManagementSystem/src/main/java/com/pxl/services/newsmanagementsystem/NewsManagementSystemApplication.java
+++ backend-jav/NewsManagementSystem/src/main/java/com/pxl/services/newsmanagementsystem/NewsManagementSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NewsManagementSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NewsManagementSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NewsManagementSystemApplication.class, args);
+    }
 
 }
