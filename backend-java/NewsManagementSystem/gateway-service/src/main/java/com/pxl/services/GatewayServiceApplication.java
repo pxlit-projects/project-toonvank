2a5573d97413debb207e555b0ea797374fa024ba
Toon <toonvankimmenade@gmail.com>
2024-11-16 14:03:30 +0100

    Gateway-Service

--- backend-java/NewsManagementSystem/gateway-service/src/main/java/com/pxl/services/GatewayServiceApplication.java
+++ backend-java/NewsManagementSystem/gateway-service/src/main/java/com/pxl/services/GatewayServiceApplication.java
@@ -2,10 +2,12 @@ package com.pxl.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-@SpringBootApplication
-/*@EnableDiscoveryClient*/
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
