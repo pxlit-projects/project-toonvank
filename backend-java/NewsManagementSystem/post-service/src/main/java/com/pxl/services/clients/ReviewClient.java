9d40a1d668853e9445e245e82ce9679083d65389
Toon <toonvankimmenade@gmail.com>
2024-12-29 23:15:55 +0100

    Update configuration for microservices; add RabbitMQ service, adjust database URLs, and enhance health checks in Docker Compose. Modify properties files for comment, post, and review services to reflect new database configurations and RabbitMQ settings. Refactor ReviewClient to remove hardcoded URL and add review queue configuration in PostService.

--- backend-java/NewsManagementSystem/post-service/src/main/java/com/pxl/services/clients/ReviewClient.java
+++ backend-java/NewsManagementSystem/post-service/src/main/java/com/pxl/services/clients/ReviewClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "review-service", url = "${review-service.url}")
+@FeignClient(name = "review-service")
 public interface ReviewClient {
     @DeleteMapping("/api/reviews/post/{postId}")
     void deleteReviewsByPostId(@PathVariable("postId") Long postId);
--- backend-java/NewsManagementSystem/post-service/src/main/java/com/pxl/services/config/QueueConfiguration.java
+++ backend-java/NewsManagementSystem/post-service/src/main/java/com/pxl/services/config/QueueConfiguration.java
@@ -1,5 +1,6 @@
 package com.pxl.services.config;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -20,6 +21,11 @@ public class QueueConfiguration {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(jsonMessageConverter());
         return rabbitTemplate;
+    }
+
+    @Bean
+    public Queue reviewQueue() {
+        return new Queue("reviewQueue", false);
     }
 
 }
